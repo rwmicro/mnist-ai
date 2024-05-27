43d3c6e5fdd14f09e4f83394d93e3dda8bc7fdaa
Martin <xvvrw@proton.me>
2024-05-27 17:33:25 +0200

    [FIX] playbook errors

--- model/BaseCNN.py
+++ model/BaseCNN.py
@@ -2,45 +2,42 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 class BaseCNN(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, num_classes=10):
         super(BaseCNN, self).__init__()
-        
-        # Convolutional layers
-        self.conv1 = nn.Conv2d(input_size, 64, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
-        self.dropout1 = nn.Dropout(0.35)
+        self.dropout1 = nn.Dropout2d(p=0.35)
         
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(128)
-        self.dropout2 = nn.Dropout(0.35)
+        self.dropout2 = nn.Dropout2d(p=0.35)
         
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(256)
-        self.maxpool1 = nn.MaxPool2d(2, 2)
-        self.dropout3 = nn.Dropout(0.35)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout3 = nn.Dropout2d(p=0.35)
         
         self.conv4 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
         self.bn4 = nn.BatchNorm2d(512)
-        self.dropout4 = nn.Dropout(0.35)
+        self.dropout4 = nn.Dropout2d(p=0.35)
         
-        self.conv5 = nn.Conv2d(512, 1024, kernel_size=3)
+        self.conv5 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
         self.bn5 = nn.BatchNorm2d(1024)
-        self.dropout5 = nn.Dropout(0.35)
+        self.dropout5 = nn.Dropout2d(p=0.35)
         
         self.conv6 = nn.Conv2d(1024, 2000, kernel_size=3, padding=1)
         self.bn6 = nn.BatchNorm2d(2000)
-        self.maxpool2 = nn.MaxPool2d(2, 2)
-        self.dropout6 = nn.Dropout(0.35)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.dropout6 = nn.Dropout2d(p=0.35)
         
-        # Fully connected layers
-        self.fc1 = nn.Linear(2000 * 2 * 2, 512)
+        self.fc1 = nn.Linear(2000 * 7 * 7, 512)
         self.bn7 = nn.BatchNorm1d(512)
-        self.dropout7 = nn.Dropout(0.5)
+        self.dropout7 = nn.Dropout(p=0.5)
         
-        self.fc2 = nn.Linear(512, output_size)
-        
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, num_classes)
+
     def forward(self, x):
-        # Convolutional layers
         x = F.relu(self.bn1(self.conv1(x)))
         x = self.dropout1(x)
         
@@ -48,7 +45,7 @@ class BaseCNN(nn.Module):
         x = self.dropout2(x)
         
         x = F.relu(self.bn3(self.conv3(x)))
-        x = self.maxpool1(x)
+        x = self.pool(x)
         x = self.dropout3(x)
         
         x = F.relu(self.bn4(self.conv4(x)))
@@ -58,16 +55,14 @@ class BaseCNN(nn.Module):
         x = self.dropout5(x)
         
         x = F.relu(self.bn6(self.conv6(x)))
-        x = self.maxpool2(x)
+        x = self.pool2(x)
         x = self.dropout6(x)
         
-        # Flatten before fully connected layers
-        x = x.view(-1, 2000 * 2 * 2)
-        
-        # Fully connected layers
+        x = x.view(-1, 2000 * 7 * 7)
         x = F.relu(self.bn7(self.fc1(x)))
         x = self.dropout7(x)
         
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         
-        return F.log_softmax(x, dim=1)
+        return x
--- model/SubNetwork.py
+++ model/SubNetwork.py
@@ -2,18 +2,19 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 class SubNetwork(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size):
         super(SubNetwork, self).__init__()
+        # Define fully connected layers for the subnetwork
         self.fc1 = nn.Linear(input_size, 512)
-        self.bn1 = nn.BatchNorm1d(512)
-        self.dropout1 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(512, output_size)
-
+        self.fc2 = nn.Linear(512, 10)  # 10 classes for MNIST/Fashion-MNIST
+        # Define batch normalization layer
+        self.bn = nn.BatchNorm1d(512)
+        # Define dropout layer
+        self.dropout = nn.Dropout(0.5)
+        
     def forward(self, x):
-        x = x.view(x.size(0), -1) 
-        x = self.fc1(x)
-        x = self.bn1(x)
-        x = F.relu(x)
-        x = self.dropout1(x)
+        # Forward pass through fully connected layers
+        x = F.relu(self.bn(self.fc1(x)))
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
